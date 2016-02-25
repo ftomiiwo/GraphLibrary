5587dc54834f5552b10775f8fca190d57799423d
Fakinlede Tomiiwo <ftomiiwo@gmail.com>
2016-02-25 10:27:02 +0100

    text spaces and method refactors

--- lib/GL.rb
+++ lib/GL.rb
@@ -1,43 +1,55 @@
 class Traverse
-    def self.dfs(graph, start, path = [])
+  
+    def self.dfs(
+      graph, 
+      start, 
+      path = []
+      )
       path = path + [start]
-      start = check_type(start)
-        if graph[start]
-          graph[start].each do |node|
-              unless path.include? node
-                path = dfs(graph, node, path)
-              end
+      start = check_type start
+      if graph[start]
+        graph[start].each do |node|
+          unless path.include? node
+            path = dfs(
+              graph,
+              node,
+              path
+              )
           end
-      end
-     path
+        end
+      end 
+      path
     end
     
-    def self.check_type(arg)
-        if arg.class == Fixnum
-          arg
+    def self.check_type arg
+      if arg.class == Fixnum
+        arg
       else
           arg= arg.to_sym
       end
     end
     
-    def self.bfs(graph, start, path = [])
-        q = [start]
+    def self.bfs(
+      graph,
+      start,
+      path = []
+      )
+      q = [start]
       until q.empty?
         v = q.shift
         unless path.include? v
-            path = path + [v]
-           v = check_type(v)
-            if graph[v]
-                q = q+ graph[v]
-            end
+          path = path + [v]
+          v = check_type v 
+          if graph[v]
+            q = q + graph[v]
+          end
         end
-        end
-        path
-    end
+      end
+      path
+  end
 end
 
 # graph = { 0 => [1,5], 1=> [0,2,3], 2=> [1,4,5], 3=> [1,2,5], 4=> [2,3,6], 5=> [0,3,4,6] } 
 graph = {'A':['B','C'],'B':['D','E'],'C':['D','E'],'D':['E'],'E':['A']}
 Traverse.bfs(graph, "A")
-
 Traverse.dfs(graph, "A")
